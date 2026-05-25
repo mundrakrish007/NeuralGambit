3ce7c18f64a24a9ba79f242993f6f0627e8d4b4b
mundrakrish007 <mundrakrish007@gmail.com>
2026-05-25 10:20:57 +0530

    scoring logic for greedy game

--- Week1/Warmup/Greedy_or_not.py
+++ Week1/Warmup/Greedy_or_not.py
@@ -1,1 +1,28 @@
-# Write your code here
+def score(nums, i, f):
+
+    if i > f:
+        return 0
+    
+    total = sum(nums[i:f+1])
+
+    left = total - score(nums, i+1, f)
+    right = total - score(nums, i, f-1)
+    return max(left, right)
+
+def main():
+    n = int(input())
+    play = list(map(int, input().split()))
+
+    total = sum(play)
+    score1 = score(play, 0, n-1)
+    score2 = total - score1
+
+    if score1 > score2:
+        print("Player 1")
+    elif score2 > score1:
+        print("Player 2")
+    else:
+        print("Draw")
+
+if __name__ == "__main__":    
+    main()
